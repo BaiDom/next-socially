8dc8017ce24e5eb12d607b16ac696c0b47018202
BaiDom <dombaily88@gmail.com>
2025-10-28 12:57:11 +0000

    navbar mob/desktop added

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,7 @@
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <div className="">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-        <SignUpButton>
-          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-            Sign Up
-          </button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className="m-4">
+      <h1>Home Page content</h1>
     </div>
   );
 }
